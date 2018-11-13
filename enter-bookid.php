23febcdfa65cfd6a8e948ca28c31952697277794
swati-kale <33409491+swati-kale@users.noreply.github.com>
2018-11-13 00:25:01 -0500

    Update enter-bookid.php

--- enter-bookid.php
+++ enter-bookid.php
@@ -17,8 +17,11 @@ li {list-style: none;}
 </html>
 <?php
 
-$connect_url = 'host=' . $_GET['dbhost'] . ' port=' . $_GET['dbport'] . ' dbname=' . $_GET['dbname'] . ' user=' . $_GET['dbuser'] . ' password=' . $_GET['dbpass'];
-echo "<br><br>Query URL = '$connect_url' <br><br>";
+/*$connect_url = 'host='. $_GET['dbhost'] . ' port=' . $_GET['dbport'] . ' dbname=' . $_GET['dbname'] . ' user=' . $_GET['dbuser'] . ' password=' . $_GET['dbpass'];
+*/
+$connect_url = 'host=mycluster.crunchydata.svc port=5432 dbname=userdb user=postgres password=SLh6Guv0qa'
+
+ echo "<br><br>Query URL = '$connect_url' <br><br>";
 
 $db = pg_connect($connect_url);
 
