7cf4d0214e5f65544d31c33b537c7ef474953e35
swati-kale <33409491+swati-kale@users.noreply.github.com>
2018-11-13 00:12:40 -0500

    Update enter-bookid.php

--- enter-bookid.php
+++ enter-bookid.php
@@ -18,7 +18,7 @@ li {list-style: none;}
 <?php
 
 $connect_url = 'host=' . $_GET['dbhost'] . ' port=' . $_GET['dbport'] . ' dbname=' . $_GET['dbname'] . ' user=' . $_GET['dbuser'] . ' password=' . $_GET['dbpass'];
-echo "<br><br>Query URL = '$connect_url' <br><br>"
+echo "<br><br>Query URL = '$connect_url' <br><br>";
 
 $db = pg_connect($connect_url);
 
